97216032c5da9c1d426d8afb3be563b4b396dd95
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-05-14 10:26:31 -0400

    updates
    adds: main pbo, verion script
    updates project.toml

---
+++ addons/main/config.cpp
@@ -0,0 +1,27 @@
+#include "script_version.hpp"
+class CfgPatches
+{
+	class 34thPC_Main
+	{
+		name = "main";
+		author = "34th Arma Company Mod Dev Team";
+		units[] = {};
+		weapons[] = {};
+		requiredAddons[] =
+		{
+
+		};
+		VERSION_CONFIG;
+	};
+};
+
+class CfgSettings
+{
+	class CBA{
+		class versioning { // https://github.com/CBATeam/CBA_A3/wiki/Versioning-System
+			class 34thPC_Main {
+				main_addon = "34thPC_Main";
+			};
+		};
+	};
+};
