ef97befb6f65617e40a4ee92f7e4083f1241d7bc
Garrett Johnson <garrettjohnson221@gmail.com>
2025-05-14 20:04:38 -0500

    TIE Fighter Configs + Faction
    title... hi

---
+++ addons/faction_phantom/config.cpp
@@ -0,0 +1,70 @@
+class CfgPatches
+{
+	class 34thPC_FactionPhantom
+	{
+		author="34th PC Modding Team";
+		units[] = {};
+		requiredAddons[] =
+		{
+		};
+	};
+};
+class CfgFactionClasses
+{
+	class 34thPC_FactionPhantom_Faction
+	{
+		displayName="34th Phantom Company";
+		author="34th PC Modding Team";
+		priority=1;
+		side=1;
+		icon="z\34thPC\addons\faction_phantom\data\company_emblem.paa";
+		flag="z\34thPC\addons\faction_phantom\data\company_emblem.paa";
+	};
+};
+class CfgEditorCategories
+{
+	class 34thPC_FactionPhantom_EditorCategory
+	{
+		displayName="34th PC Phantom";
+	};
+};
+class CfgEditorSubcategories
+{
+	class 34thPC_FactionPhantom_EditorSubCategory_Aircraft
+	{
+		displayName="Aircraft";
+	};
+	class 34thPC_FactionPhantom_EditorSubCategory_Cars
+	{
+		displayName="Cars";
+	};
+	class 34thPC_FactionPhantom_EditorSubCategory_Tanks
+	{
+		displayName="Tanks";
+	};
+	class 34thPC_FactionPhantom_EditorSubCategory_APCs
+	{
+		displayName="APCs";
+	};
+	class 34thPC_FactionPhantom_EditorSubCategory_IFVs
+	{
+		displayName="IFVs";
+	};
+	// Not sure about these, maybe split marines and ODST?
+	class 34thPC_FactionPhantom_EditorSubCategory_Infantry_Woodland
+	{
+		displayName="Troopers Woodland";
+	};
+	class 34thPC_FactionPhantom_EditorSubCategory_Infantry_Urban
+	{
+		displayName="Troopers Urban";
+	};
+	class 34thPC_FactionPhantom_EditorSubCategory_Infantry_Snow
+	{
+		displayName="Troopers Snow";
+	};
+	class 34thPC_FactionPhantom_EditorSubCategory_Infantry_Arid
+	{
+		displayName="Troopers Snow";
+	};
+};
--- mod.cpp
+++ mod.cpp
@@ -1,7 +1,7 @@
-name = "34th PRC Aux Mod";
+name = "34th PC Aux Mod";
 picture = "34thPRC_logo.paa";
-overview = "The aux mod for the 34th Para-Recon ""Brimstone"" Company.<br/>You probably only need this if you are in our unit.";
+overview = "The aux mod for the 34th""Phantom"" Company.<br/>You probably only need this if you are in our unit.";
 logo = "34thPRC_logo.paa";
 logoOver = "34thPRC_logo.paa";
 actionName = "GitHub";
-action = "https://github.com/34th-Para-Recon-Brimstone-Company/34th-PRC-Aux-Mod";
+action = "https://github.com/34th-Arma-Company/Phantom-Company-Aux-Mod";
