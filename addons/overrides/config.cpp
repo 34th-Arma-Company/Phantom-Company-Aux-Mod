8b3b5d7351eeb7b332168148772b56d12754809f
Garrett Johnson <garrettjohnson221@gmail.com>
2025-05-20 10:13:32 -0500

    tie-test-suite (#15)
    @OverYandere gib pull pls
    once we can post the dev addon, we can have Jen and Vas test the flight
    aspects of the tie tests.

--- addons/overrides/config.cpp
+++ addons/overrides/config.cpp
@@ -5,12 +5,21 @@ class CfgPatches
 		author="34th PC Modding Team";
 		units[]=
 		{
-			"34thPC_TieLN",
+			"34thPC_TieLN_1",
+			"34thPC_TieLN_2",
+			"34thPC_TieLN_3",
+			"34thPC_TieLN_4",
+			"34thPC_TieLN_5",
+			"34thPC_TieLN_6",
+			"34thPC_TieLN_7",
+			"34thPC_TieBomber_1",
+			"34thPC_TieBomber_2",
 		};
 		requiredAddons[] =
 		{
 			"WM_Imperial_Vehicles",
 			"34thPC_FactionPhantom",
+			"3AS_Imperial_Air",
 		};
 	};
 };
