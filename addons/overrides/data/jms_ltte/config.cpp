b4ec21b25f42fcca3cf13a667f96fb8170354a5c
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2025-08-07 06:47:44 -0400

    Config and filepath setup

---
+++ addons/overrides/data/jms_ltte/config.cpp
@@ -0,0 +1,19 @@
+class CfgPatches
+{
+	class 34thPC_jms_llte_patches
+	{
+		author="Phantom Company Modding Team, Vasya";
+		addonRootClass="34thPC_Overrides";
+		requiredAddons[]=
+		{
+			"34thPC_Overrides",
+			"JMSLLTE_weapons",
+			"3AS_Weapons",
+		};
+	};
+};
+
+class CfgWeapons
+{
+	#include "config_weapons.hpp"
+};
---
+++ addons/overrides/data/jms_ltte/weapons/tl50/config_weapons.cpp
@@ -0,0 +1,11 @@
+class JMSLLTE_tl50; //JMS Weapons TL50
+class 3AS_45Rnd_EC50_Mag; //3AS Weapons
+
+    class JMSLLTE_tl50
+    {
+    magazines[]=
+		{
+			"JMSLLTE_Tl50_40rnd_Mag"
+            "3AS_45Rnd_EC50_Mag"
+		};
+    }
