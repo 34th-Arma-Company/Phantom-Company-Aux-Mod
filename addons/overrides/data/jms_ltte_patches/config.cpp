41d064432774cd74ff675728b00bf731b1d7a640
OverYandere <126543615+OverYandere@users.noreply.github.com>
2025-08-18 11:23:09 -0400

    Feature/jms rebalance (#30)
    Creates new ammo for the TL 50 that works appropriately with our balance
    systems.
    
    ---------
    
    Co-authored-by: Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>

---
+++ addons/overrides/data/jms_ltte_patches/config.cpp
@@ -0,0 +1,15 @@
+class CfgPatches
+{
+	class 34thPC_jms_llte_patches
+	{
+		author="Phantom Company Modding Team, Vasya";
+		addonRootClass="34thPC_Overrides";
+		requiredAddons[]=
+		{
+			"34thPC_Overrides",
+			"JMSLLTE_weapons",
+			"JMSLLTE_wep_mags",
+			"3AS_Weapons",
+		};
+	};
+};
--- changelog.md
+++ changelog.md
@@ -11,6 +11,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - 3AS Imperial Scout Drone now dissasembles into the new backpack
 ### Updated
 - Many drugs, and splints reskinned
+- TL50 ammo adjusted to provide better results
 
 ## 0.0.3
 ### Fixed
