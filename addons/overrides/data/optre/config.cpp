a5ac921d66079d89e74c021d743c0426b23073bc
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2025-09-16 09:47:56 -0400

    Feature/vehicle retextures (#36)
    REVISION 1 textures & config have been completed for the **[34th]
    ARC-170 (Imperial)** & **[34th] Imperial-class AG-134 Star Sparrow**
    
    ---------
    
    Co-authored-by: OverYandere <126543615+OverYandere@users.noreply.github.com>

---
+++ addons/overrides/data/optre/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches
+{
+	class 34thPC_optre_patches
+	{
+		author="Phantom Company Modding Team, Yandere";
+		addonRootClass="34thPC_Overrides";
+		requiredAddons[]=
+		{
+			"34thPC_Overrides",
+		};
+		units[]={
+		};
+	};
+};
---
+++ addons/overrides/data/optre/sparrowhawk/config.cpp
@@ -0,0 +1,52 @@
+class CfgPatches
+{
+	class 34thPC_Sparrowhawk
+	{
+		author="Phantom Company Modding Team, Yandere, Vasya";
+		addonRootClass="34thPC_optre_patches";
+		requiredAddons[]=
+		{
+			"34thPC_optre_patches",
+			"OPTRE_Vehicles_Sparrowhawk",
+		};
+		units[]={
+			"34thPC_Imperial_StarWing"
+		};
+	};
+};
+
+class CfgVehicles
+{
+	class OPTRE_AV22A_Sparrowhawk;
+	class 34thPC_Sparrowhawk_Base : OPTRE_AV22A_Sparrowhawk
+	{
+		scope=0;
+		dlc="34th";
+		author="34thPC Mod Dev Team";
+		displayName="34th Star Wing Base";
+		//Do changes for hud and or flight model here
+	};
+
+	class 34thPC_Imperial_StarWing : 34thPC_Sparrowhawk_Base
+	{
+		author="34thPC Mod Dev Team";
+		displayName="[34th] Imperial-class AG-134 Star Sparrow";
+		editorPreview="";
+		scope=2;
+		scopeCurator=2;
+		crew="3AS_Imperial_TIE_Pilot_F";
+		side=1;
+		faction="34thPC_FactionPhantom_Faction";
+		hiddenSelectionsTextures[] =
+		{
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Body_01_co.paa",
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Body_02_co.paa",
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Body_03_co.paa",
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Body_04_co.paa",
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Body_05_CO.paa",
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Autocannon_CO.paa",
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Feed_Autocannon_CO.paa",
+			"\z\34thpc\addons\overrides\data\optre\sparrowhawk\textures\Heavy_Autocannon_CO.paa",
+		};
+	};
+};
