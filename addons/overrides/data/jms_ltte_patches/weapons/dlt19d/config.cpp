b4556705343bbdc7b96442ac63eba5ffa352dec1
Vazya <161765906+Vazya-H3lljumper@users.noreply.github.com>
2025-09-17 03:00:43 -0400

    Rebalance

--- addons/overrides/data/jms_ltte_patches/weapons/dlt19d/config.cpp
+++ addons/overrides/data/jms_ltte_patches/weapons/dlt19d/config.cpp
@@ -1,12 +1,29 @@
-/*class CfgPatches
+class CfgPatches
 {
-	class 34thPC_jms_dlt19d //Edit if name wrong and remove comment when done
+	class 34thPC_jms_DLT19D
 	{
-		author="Phantom Company Modding Team, Vasya";
+		author="Phantom Company Modding Team, Yandere, Vasya";
 		addonRootClass="34thPC_jms_llte_patches";
 		requiredAddons[]=
 		{
 			"JMSLLTE_weapons",
+			"3AS_Weapons"
+		};
+		units[] =
+		{
+			"JMSLLTE_DLT19dRifle",
 		};
 	};
-};*/
+};
+
+class CfgWeapons
+{
+	class JMSLLTE_BlasterRifle_Base;
+	class JMSLLTE_DLT19dRifle: JMSLLTE_BlasterRifle_Base
+	{
+		magazines[]+=
+		{
+			"3AS_10Rnd_EM100_DLT19X_Mag"
+		};
+	};
+};
--- addons/overrides/data/jms_ltte_patches/weapons/e11d/config.cpp
+++ addons/overrides/data/jms_ltte_patches/weapons/e11d/config.cpp
@@ -1,12 +1,29 @@
-/*class CfgPatches
+class CfgPatches
 {
-	class 34thPC_jms_e11d //Edit if name wrong and remove comment when done
+	class 34thPC_jms_E11D
 	{
-		author="Phantom Company Modding Team, Vasya";
+		author="Phantom Company Modding Team, Yandere";
 		addonRootClass="34thPC_jms_llte_patches";
 		requiredAddons[]=
 		{
 			"JMSLLTE_weapons",
+			"3AS_Weapons"
+		};
+		units[] =
+		{
+			"JMSLLTE_e11d",
 		};
 	};
-};*/
+};
+
+class CfgWeapons
+{
+	class JMSLLTE_BlasterRifle_Base;
+	class JMSLLTE_e11d: JMSLLTE_BlasterRifle_Base
+	{
+		magazines[]+=
+		{
+			"3AS_45Rnd_EM50_Mag"
+		};
+	};
+};
